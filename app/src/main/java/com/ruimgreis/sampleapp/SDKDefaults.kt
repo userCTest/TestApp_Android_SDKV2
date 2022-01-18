151fbdf239b82528ad2c57ba3c4da649eaf35a79
Rui Reis <rui.reis@usercentrics.com>
2022-01-18 09:29:03 +0000

    Changed settingsId for a demo account

--- app/src/main/java/com/ruimgreis/sampleapp/SDKDefaults.kt
+++ app/src/main/java/com/ruimgreis/sampleapp/SDKDefaults.kt
@@ -4,7 +4,7 @@ class SDKDefaults {
 
     /**
      * Default values for properties in SDK
-     * example settingsId = "bsKaFY7cM", //"ZDQes7xES", "egLMgjg9j", "PaagpXyrs"
+     * example settingsId = "ZDQes7xES", "egLMgjg9j"
      */
 
     var controllerID: String? = null //"1727720271ab52d129c92f7b9ee5bf792c6d7b90489d9fa6d36de0ffbb83dc1c"
